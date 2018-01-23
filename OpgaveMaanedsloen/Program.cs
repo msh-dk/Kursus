24237fd942c20666f127826525771e9e41265c8f
Mads Sander Hansen <msh@connect2cloud.dk>
2018-01-23 10:47:56 +0100

    Tilføjelse af nyt projekt OpgaveMaanedsloen.

---
+++ OpgaveMaanedsloen/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveMaanedsloen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salaryTotal = 0;
+            double gns = 0;
+            int[] salaryArray = { 20000, 34932, 37293, 84722, 28746, 27467 };
+
+            for (int i = 0; i < salaryArray.Length; i++)
+            {
+                salaryTotal += salaryArray[i];
+                Console.WriteLine("Månedsløn nr. " + (i+1) + ": " + salaryArray[i].ToString("N2"));
+            }
+
+            if (salaryArray.Length > 0)
+            {
+                gns = salaryTotal / salaryArray.Length;
+            }
+
+            Console.WriteLine("Gennemsnit: " + gns.ToString("N2"));
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
