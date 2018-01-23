348c580c5c7a38be423f2d906c8a336880476ed5
Mads Sander Hansen <msh@connect2cloud.dk>
2018-01-23 13:13:39 +0100

    Opgave10Tabel projekt oprettet

---
+++ Opgave10Tabel/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave10Tabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] numbers2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            string row = "";
+
+            foreach (var item in numbers1)
+            {
+
+                row = "";
+                
+                for (int i = 0; i < numbers2.Length; i++)
+                {
+                    int sum = 0;
+                    sum = item * numbers2[i];
+                    if (sum > 50)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+     
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                      }
+                    Console.Write(sum.ToString().PadLeft(4));
+            
+                }
+                Console.WriteLine();
+
+            }
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
