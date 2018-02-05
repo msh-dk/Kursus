b1a34a0958875e79ae246de857eb47d2bd93c681
Mads Sander Hansen <msh@connect2cloud.dk>
2018-02-05 11:13:31 +0100

    nyt project OpgaveSimpleClass

---
+++ OpgaveSimpleClass/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveSimpleClass
+{
+   public class Person
+    {
+        public string Fornavn;
+        public string Efternavn;
+        public int Fødselsår;
+        
+        public Person():this("", "", 0)
+        {}
+
+        public Person(string fornavn, string efternavn, int fødselsår)
+        {
+            this.Fornavn = fornavn.ToUpper();
+            this.Efternavn = efternavn.ToUpper();
+            this.Fødselsår = fødselsår;
+        }
+
+        public string FuldtNavn()
+        {
+            return this.Fornavn + " " + this.Efternavn;
+        } //FuldtNavn()
+
+        public int Alder()
+        {
+            int year = DateTime.Today.Year;
+            return year - Fødselsår;
+
+        }// Alder()
+
+
+    }
+}
---
+++ OpgaveSimpleClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveSimpleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person a = new Person("a", "b");
+            string a_navn = a.FuldtNavn();
+
+            Person b = new Person("b", "c", 1967);
+            int alder = b.Alder();
+
+        }
+    }
+}
