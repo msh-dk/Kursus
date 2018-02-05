849bca51e3ee9263c117888e921279778d189da7
Mads Sander Hansen <msh@connect2cloud.dk>
2018-02-05 15:43:58 +0100

    Div projects

---
+++ OpgaveArealBeregninger.cs/ArealBeregninger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveArealBeregninger.cs
+{
+    public static class ArealBeregninger
+    {
+        public static double BeregnArealFirkant(double højde, double bredde)
+        {
+            return højde * bredde;
+        }
+
+        public static double BeregnArealCirkel(double radius)
+        {
+            return System.Math.PI * (radius * radius);
+
+        }
+    }
+}
---
+++ OpgaveArealBeregninger.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveArealBeregninger.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArealBeregninger.BeregnArealFirkant(10, 10));
+            Console.WriteLine(ArealBeregninger.BeregnArealCirkel(10));
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+
+
+    }
+}
