fe699dfe788a47ecbf13cab73c9d65cf1835e3c8
Mads Sander Hansen <msh@connect2cloud.dk>
2018-02-06 09:29:13 +0100

    Nyt Opg projekt.

---
+++ OpgaveVare/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveVare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vare v1 = new Vare();
+            double p1 = v1.Pris;
+            v1.Pris = 5.45;
+            v1.Navn = "Vingummi";
+
+            string n1 = v1.Navn;
+
+            Vare v2 = new Vare("Chocolade", 45.78);
+            double p2 = v2.Pris;
+            string n2 = v2.Navn;
+            double p2_moms = v2.PrisMedMoms();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ OpgaveVare/Vare.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveVare
+{
+    public class Vare
+    {
+        private double pris;
+
+        public double Pris
+        {
+            get {
+                Console.WriteLine("Property Pris. Get: " + pris);
+                return pris; }
+            set {
+                Console.WriteLine("Property Pris. Set: " + value);
+                pris = value; }
+        }
+
+        private static double momsPct = 0.25;
+
+        private string navn;
+
+        public string Navn
+        {
+            get {
+                Console.WriteLine("Property Navn. Get: " + navn);
+
+                return navn; }
+            set
+            {
+                Console.WriteLine("Property Navn. Set: " + value);
+                navn = value; }
+        }
+
+
+        public Vare():this("",0.0)
+        {
+
+        }
+        public Vare(string vareNavn, double pris)
+        {
+            this.Navn = vareNavn;
+            this.Pris = pris;
+        }
+
+        public double PrisMedMoms()
+        {
+            double res = this.Pris * (1 + momsPct);
+            Console.WriteLine("Method PrisMedMoms. " + res);
+            return res;
+        }
+    }
+}
